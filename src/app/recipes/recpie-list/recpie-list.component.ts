0dfb3b125a4cf40f211006e61edf92ac0cdd83ed
MeTi-DeV <mahdi_zarei78@yahoo.com>
2021-12-18 12:16:51 +0330

    add Model of recipe and objects inside it

--- src/app/recipes/recpie-list/recpie-list.component.ts
+++ src/app/recipes/recpie-list/recpie-list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+// comment 1 : add recpie.model to recpie-list
+import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recpie-list',
   templateUrl: './recpie-list.component.html',
-  styleUrls: ['./recpie-list.component.css']
+  styleUrls: ['./recpie-list.component.css'],
 })
 export class RecpieListComponent implements OnInit {
+  //component 2 : here define recpie array and create new object of
+  //recpie.model with values
+  recipe: Recipe[] = [
+    new Recipe(
+      'Recipe Test',
+      'Recipe Test Description',
+      'https://www.licious.in/blog/wp-content/uploads/2020/12/Pan-Fried-Chicken.jpg'
+    ),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
